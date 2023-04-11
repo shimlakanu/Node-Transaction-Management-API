7a4d001d7cb30166b693259e2d509c9a0c0f9078
ShimlaKanu <shimlakanu@pop-os.localdomain>
2023-04-12 03:59:33 +0600

    cash-in and cash-out management

--- index.js
+++ index.js
@@ -11,98 +11,161 @@ let cur_id = 1;
 
 
 
-app.post('/account', async(req, res) => {
+app.post('/account', async (req, res) => {
     const name = req.body.name;
     const password = req.body.password;
-    let account_id = uuid.v4(); 
+    let account_id = uuid.v4();
 
-    const {data, error} = await supabase
-        .from ('Account')
+    const { data, error } = await supabase
+        .from('Account')
         .insert({
-            'account_id' : account_id,
-            'id' : cur_id++,
-            'created_at' : new Date().toJSON(),
-            'password' :  password,
-            'name' : name,
-            "balance" : 0,
-            'reward_point' : 0
+            'account_id': account_id,
+            'id': cur_id++,
+            'created_at': new Date().toJSON(),
+            'password': password,
+            'name': name,
+            "balance": 0,
+            'reward_point': 0
         });
 
-    if(!error) 
-    {
+    if (!error) {
         res.send("successfully done :D ")
     }
-    else 
-    {
+    else {
         res.send(" please recheck ")
     }
-    
+
 })
 
 
-app.get('/Account/:id', async (req,res) => {
+app.get('/Account/:id', async (req, res) => {
     const id = parseInt(req.params.id);
     const { data, error } = await supabase
         .from('Account')
         .select()
         .eq('id', id);
 
-    if (!error) 
-    {
+    if (!error) {
         const response = {
-            "name " : data[0].name,
-            "id" : data[0].id,
-            "created_at" : data[0].created_at,
-            "reward_point" : data[0].reward_point,
-            "balance" : data[0].balance
+            "name ": data[0].name,
+            "id": data[0].id,
+            "created_at": data[0].created_at,
+            "reward_point": data[0].reward_point,
+            "balance": data[0].balance
         }
         res.json(response);
     }
-    else 
-    {
+    else {
         res.send("no such id exists");
     }
 })
 
 
-app.get('/Account/:id/balance', async (req,res) => {
+app.get('/Account/:id/balance', async (req, res) => {
     const id = parseInt(req.params.id);
     const { data, error } = await supabase
         .from('Account')
         .select()
         .eq('id', id);
 
-    if (!error) 
-    {
+    if (!error) {
         const response = {
-            "balance" : data[0].balance
+            "balance": data[0].balance
         }
         res.json(response);
     }
-    else 
-    {
+    else {
         res.send("no such id exists");
     }
 })
 
-app.get('/Account/:id/reward_point', async (req,res) => {
+app.get('/Account/:id/reward_point', async (req, res) => {
     const id = parseInt(req.params.id);
     const { data, error } = await supabase
         .from('Account')
         .select()
         .eq('id', id);
 
-    if (!error) 
-    {
+    if (!error) {
         const response = {
-            "reward_point" : data[0].reward_point
+            "reward_point": data[0].reward_point
         }
         res.json(response);
     }
-    else 
-    {
+    else {
         res.send("no such id exists");
     }
 })
+
+
+app.put('/cash_in', async (req, res) => {
+    const amount = req.body.amount;
+    const id = req.body.id;
+    const password = req.body.password;
+    const { data, error } = await supabase
+        .from('Account')
+        .select()
+        .eq('id', id);
+
+    if (!error) {
+        const actual_password = data[0].password;
+        const current_balance = data[0].balance;
+
+        if (actual_password == password) {
+            const { data, error } = await supabase
+                .from('Account')
+                .update({ 'balance': current_balance + amount })
+                .eq('id', id);
+            res.send("Successfully done :D");
+        }
+        else {
+            res.send("Incorrect password. Please try again :D")
+        }
+    }
+    else {
+        res.send("No such account exist :D");
+    }
+})
+
+
+app.put('/cash_out', async (req, res) => {
+    const amount = req.body.amount;
+    const id = req.body.id;
+    const password = req.body.password;
+    const { data, error } = await supabase
+        .from('Account')
+        .select()
+        .eq('id', id);
+
+    if (!error) {
+        const actual_password = data[0].password;
+        const current_balance = data[0].balance;
+
+        if (actual_password == password) {
+            if (current_balance >= amount) {
+                const { data, error } = await supabase
+                    .from('Account')
+                    .update({ 'balance': current_balance - amount })
+                    .eq('id', id);
+                res.send("Successfully done :D");
+            }
+            else 
+            {
+                res.send("Insufficient balance :D ");
+            }
+        }
+        else {
+            res.send("Incorrect password. Please try again :D")
+        }
+    }
+    else {
+        res.send("No such account exist :D");
+    }
+})
+
+
+// app.post('/transaction', async (req,res)=>{
+
+// })
 
 app.listen(port);
